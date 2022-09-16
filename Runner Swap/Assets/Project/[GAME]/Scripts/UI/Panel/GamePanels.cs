6690411be86d45b1266d234730da1a5ef05b1830
Beyzanur <beyzaalways@gmail.com>
2022-09-16 22:35:59 +0300

    UI scene is almost done
    draft for ui scene is ready.
    some animation controls are changed.
    modern ui package is imported.

--- Runner Swap/Assets/Project/[GAME]/Scripts/UI/Panel/GamePanels.cs
+++ Runner Swap/Assets/Project/[GAME]/Scripts/UI/Panel/GamePanels.cs
@@ -4,44 +4,44 @@ using UnityEngine;
 
 public class GamePanels : Panel
 {
-    public Panel LevelSuccesPanel;
-    public Panel LevelFailPanel;
-    public Panel BlurPanel;
+    public Panel GameStartPanel;
+    public Panel GameEndPanel;
+    //public Panel //BlurPanel;
 
     private void Awake() 
     {
-        LevelSuccesPanel.HidePanel();
-        LevelFailPanel.HidePanel();
-        BlurPanel.HidePanel();
+        GameStartPanel.ShowPanel();
+        GameEndPanel.HidePanel();
+        //BlurPanel.HidePanel();
     }
 
     private void OnEnable()
     {
-        EventManager.OnLevelFail.AddListener(InitializeLevelFailPanel);
-        EventManager.OnLevelSuccess.AddListener(InitializeLevelSuccessPanel);
-        EventManager.OnGameStart.AddListener(HidePanel);
+        EventManager.OnLevelFail.AddListener(InitializeGameEndPanel);
+        /*EventManager.OnLevelSuccess.AddListener(InitializeLevelSuccessPanel);
+        EventManager.OnGameStart.AddListener(HidePanel);*/
     }
 
     private void OnDisable()
     {
-        EventManager.OnLevelFail.RemoveListener(InitializeLevelFailPanel);
-        EventManager.OnLevelSuccess.RemoveListener(InitializeLevelSuccessPanel);
-        EventManager.OnGameStart.RemoveListener(HidePanel);
+        EventManager.OnLevelFail.RemoveListener(InitializeGameEndPanel);
+        /*EventManager.OnLevelSuccess.RemoveListener(InitializeLevelSuccessPanel);
+        EventManager.OnGameStart.RemoveListener(HidePanel);*/
     }
 
-    private void InitializeLevelFailPanel()
+    private void InitializeGameEndPanel()
     {
-        LevelSuccesPanel.HidePanel();
-        BlurPanel.ShowPanel();
-        LevelFailPanel.ShowPanel();
+        GameStartPanel.HidePanel();
+        //BlurPanel.ShowPanel();
+        GameEndPanel.ShowPanel();
         ShowPanel();
     }
 
-    private void InitializeLevelSuccessPanel()
+    /*private void InitializeLevelSuccessPanel()
     {
-        LevelSuccesPanel.ShowPanel();
-        BlurPanel.ShowPanel();
-        LevelFailPanel.HidePanel();
+        GameStartPanel.ShowPanel();
+        //BlurPanel.ShowPanel();
+        GameEndPanel.HidePanel();
         ShowPanel();
-    }
+    }*/
 }
