412feccbfcfdd5c42defd26ef3ee8484d56819d9
Beyzanur <beyzaalways@gmail.com>
2022-10-11 20:18:04 +0300

    Working on Audio

---
+++ Runner Swap/Assets/Project/[GAME]/Scripts/Audio/InGameFX/AudioManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.spatialBlend = s.spatialBlend;
+        }
+    }
+
+    void OnEnable()
+    {
+        EventManager.OnCoinPickUp.AddListener(PlayFX);
+    }
+    void OnDisable()
+    {
+        EventManager.OnCoinPickUp.RemoveListener(PlayFX);
+    }
+
+    void PlayFX()
+    {
+        Play("Chicks");
+    }
+
+    public static bool isSoundOn = true;
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Sound: " + name + "not found!");
+            return;
+        }
+        isSoundOn = true;
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Sound: " + name + "not found!");
+            return;
+        }
+        s.source.Stop();
+        isSoundOn = false;
+    }
+}
